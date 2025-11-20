fa7eae958140b7370b3815621146dddf14c322e1
Sofia Garzon <s.garzonm2@uniandes.edu.co>
2025-11-20 07:49:37 -0500

    cambios2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MovieService } from './movie/movie.service';
-
-
-const routes: Routes = [];
-
+import { MovieListComponent } from './movie/movie-list/movie-list.component';
+import { MovieDetailComponent } from './movie/movie-detail/movie-detail.component';
+const routes: Routes = [
+  { path: 'movie', component: MovieListComponent },
+  { path: 'movie/:id', component: MovieDetailComponent },
+  { path: '', redirectTo: '/movie', pathMatch: 'full' }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
